79ff7fd7558601c94e2260ede69df65f336b4422
wenjun.li <wenjunli427@163.com>
2021-09-13 12:34:07 +0800

    测试

---
+++ src/test/java/com/liwenjun/sprngbootstudy/test/Test4.java
@@ -0,0 +1,11 @@
+package com.liwenjun.sprngbootstudy.test;
+
+/**
+ * @ClassName Test4
+ * @Description TODO
+ * @Author liwenjun
+ * @Date 2021/9/13 12:33 下午
+ * @Version: 1.0
+ */
+public class Test4 {
+}
